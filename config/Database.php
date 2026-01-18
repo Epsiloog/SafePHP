bb3696ded418f4d50ea66a3e80d021479498b4c9
Thomas.Th <thomas.thony.69@gmail.com>
2026-01-18 14:17:39 +0100

    [UPDATE] Fix de l'implémentation de Dotenv

--- config/Database.php
+++ config/Database.php
@@ -5,11 +5,16 @@ use PDOException;
 use Dotenv\Dotenv;
 //Utiliser le .env
 class Database {
-    public static function connectDatabase($host, $dbname, $name, $password, $port) {
+    public static function connectDatabase() {
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
+        $host = $_ENV["HOST"];
+        $port = $_ENV["PORT"];
+        $dbname = $_ENV["DB_NAME"];
+        $user_name = $_ENV["USER_NAME"];
+        $password = $_ENV["PASSWORD_DB"];
         try {
-            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8; port=$port", $name, $password);
+            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8; port=$port", $user_name, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch (PDOException $e) {
